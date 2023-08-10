21d98806552e933b74e5eaaada3abe279708f198
Vinicius Abreu <vs.abreu@hotmail.com>
2023-08-09 23:16:13 -0300

    feat: hello world

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -3,6 +3,7 @@ import { userSession } from "./user/session.routes";
 
 const routes = Router()
 
+routes.get('/', (_, res) => res.status(200).send('Hello world!'));
 routes.use('/user', userSession)
 
 export { routes };
